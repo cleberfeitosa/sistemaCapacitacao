b5bdd021514e2a3ddb935f24840c93576e72da04
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2025-05-30 10:21:19 -0400

    VarArgs

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -10,8 +10,13 @@ public class Programa {
         isabela.setOrgao("ANVISA");
         isabela.setLotacao("Brasilia");
         isabela.setEmail("isabela@gmail.");
+        isabela.setNome("João");;
+
+        isabela.calcularSalarioHorasExtras(10,5.60);
 
         System.out.println("Servidor: "+ isabela.getNome());
+        System.out.printf("Horas extras R$ %.2f: ",isabela.getHorasExtras());
+        isabela.calcularNumeros(4,6,20,56);
 
     }
 }
